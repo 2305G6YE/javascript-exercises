6ce46e087ec2992a1ba4724f134e9995856ae7cf
2305G6YE <ulmi910@gmail.com>
2025-05-22 09:49:17 +0900

    COMPLETE: 08 solution add and test

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,8 +1,11 @@
 const reverseString = function(string) {
-let arr = string.split('');
+/*
+    let arr = string.split('');
 let revArr = arr.reverse();
 return revArr.join("");
+*/
 
+return string.split("").reverse().join("");
 
 };
 
