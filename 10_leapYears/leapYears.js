e54e672e2822dcb890cccb1e9d479db1e0e6fb34
2305G6YE <ulmi910@gmail.com>
2025-05-22 11:20:49 +0900

    10 exercise solution add and test

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,26 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //Assesses if divisible by 4
+     //IF yes;
+    if (year % 4 === 0) {
+        //If can be divided by 100
+        if (year % 100 === 0) {
+            //IF yes;
+            //IF divisible by 400
+            if (year % 400 === 0) {
+                return true;
+            } else {
+                //returns falsSe
+                return false;
+            }
 
+        } else {
+            return true;
+        }
+    } else {
+        //IF no;
+        //returns false;
+        return false;
+    }
 };
 
 // Do not edit below this line
