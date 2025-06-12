15212a98954445e44a6b5f9c1be1441b75916a61
2305G6YE <ulmi910@gmail.com>
2025-06-12 11:10:31 +0900

    update old files

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+/*
+const removeFromArray = function(arr, ...args) {
+const newArray = [];
+array.forEach((item) => {
+    if (!args.includes(item)) {
+        newArray.push(item);
+    }
+});
+return newArray; */
+
+const removeFromArray = function(arr, ...args) {
+    return arr.filter((item) => !args.includes(item))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
