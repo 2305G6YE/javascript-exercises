c9fbd7435b52e35f1794b2d534927a8b3396aada
2305G6YE <ulmi910@gmail.com>
2025-07-18 12:16:18 +0900

    ADD: entry solution for exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
-};
+const palindromes = function (input) {
+let lowerCase = input.toLowerCase();
+let withoutPunc = lowerCase.replace (/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+let finalString = withoutPunc.replace((/\s{2,}/g," "));
+let arr = Array.from(finalString);
+let reverseArr = arr.reverse();
+if (arr = reverseArr) {
+    return true;
+} else {
+    return false;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
