517825b772ae44c1092d966104d064df7b14a661
2305G6YE <ulmi910@gmail.com>
2025-07-18 11:54:01 +0900

    ADD: Solution for exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -11,17 +11,26 @@ const sum = function(numbers) {
   return arr.reduce((prev,next) => prev + next, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let arr = Array.from(numbers);
+  return arr.reduce((prev,next) => prev * next, 1); 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(number) {
+ let arr = [];
+ while (number > 0) {
+  arr.push(number);
+  number--;
+ }
+  if (number === 0 && arr.length === 1) {
+    return 1;
+  } else
+      return arr.reduce((prev,next) => prev * next, 1)
+  };
 
 // Do not edit below this line
 module.exports = {
